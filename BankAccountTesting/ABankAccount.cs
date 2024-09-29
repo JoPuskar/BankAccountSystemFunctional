3c9bf314a138fbd88dfb50f8fa2d7f2f87a04dd5
Joshi <joship1@etsu.edu>
2024-09-29 16:13:20 -0400

    Functional test for Boundary values added

--- BankAccountTesting/ABankAccount.cs
+++ BankAccountTesting/ABankAccount.cs
@@ -117,6 +117,7 @@ namespace BankAccountTesting
 
             // Assert
             Assert.That(sut.Balance, Is.EqualTo(40m));
+            Assert.That(_recipient.Balance, Is.EqualTo(80m));
 
 
         }
@@ -177,5 +178,96 @@ namespace BankAccountTesting
             });
 
         }
+
+        // Boundary Value Analysis
+
+        [Test]
+        public void ShouldInitializeBankAccountWithBoundaryValuesAsInitialBalance()
+        {
+            // Passing 0
+            // Arrange
+            string accountNumber = "123456";
+            decimal initialBalance = 0m;
+            
+            // Act
+            BankAccount sut = new BankAccount(accountNumber, initialBalance);
+
+            // Assert
+            Assert.That(sut.Balance, Is.EqualTo(0m));
+
+            // Passing a very small positive value
+            // Arrange 
+            initialBalance = 0.00000005m;
+
+            // Act
+            sut = new BankAccount(accountNumber, initialBalance);
+
+            // Assert
+            Assert.That(sut.Balance, Is.EqualTo(0.00000005m));
+
+            // Passing a very large positive value
+            // Assert 
+            initialBalance = decimal.MaxValue;
+
+            // Act
+            sut = new BankAccount(accountNumber, initialBalance);
+
+            // Assert
+            Assert.That(sut.Balance, Is.EqualTo(decimal.MaxValue));
+
+        }
+
+        [Test]
+        public void ShouldIncreaseBalanceWhenBoundaryValueAmountsAreDeposited()
+        {
+            // Arrange
+            string accountNumber = "123456";
+            decimal initialBalance = 0m;
+
+            // Act
+            BankAccount sut = new BankAccount(accountNumber, initialBalance);
+            // Depositing a very small amount
+            sut.Deposit(0.001m);
+
+            // Assert
+            Assert.That(sut.Balance, Is.EqualTo(0.001m));
+
+            // Depositing a very large positive value
+
+            sut = new BankAccount(accountNumber, initialBalance);
+
+            sut.Deposit(5000000000900000m);
+
+            // Assert
+            Assert.That(sut.Balance, Is.EqualTo(5000000000900000m));
+
+        }
+
+        [Test]
+        public void ShouldDecreaseBalanceWhenBoundaryValueAmountsAreWithdrawed()
+        {
+            // Arrange
+            string accountNumber = "123456";
+            decimal initialBalance = 100m;
+
+            // Act
+            BankAccount sut = new BankAccount(accountNumber, initialBalance);
+            // Depositing a very small amount
+            sut.Withdraw(0.001m);
+
+            // Assert
+            Assert.That(sut.Balance, Is.EqualTo(99.999m));
+
+            // Depositing a very large positive value
+
+            sut = new BankAccount(accountNumber, 5000000000900000m);
+
+            sut.Withdraw(5000000000900000m);
+
+            // Assert
+            Assert.That(sut.Balance, Is.EqualTo(0m));
+
+        }
+
     }
 }
